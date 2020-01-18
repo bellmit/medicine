8de5129cbb527f07ea07ad25a25628ce99779ad2
freedom-g <15502283132@163.com>
2020-01-18 22:41:02 +0800

    添加购物车的实现类

---
+++ pharmacy-common/src/main/java/vo/CartItem.java
@@ -0,0 +1,70 @@
+package vo;
+
+import java.util.Date;
+
+/**
+ * Created by lenovo on 2020/1/18.
+ */
+public class CartItem {
+
+    private Integer id;
+
+    private Integer uid;
+
+    private Integer itemId;
+
+    private Integer number;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
