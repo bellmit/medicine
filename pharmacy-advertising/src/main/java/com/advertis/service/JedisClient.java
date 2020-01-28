0ef9044152c8e6871d1361a59c6091edd0246b22
freedom-g <15502283132@163.com>
2020-01-28 20:38:26 +0800

    创建类

---
+++ pharmacy-advertising/src/main/java/com/advertis/service/JedisClient.java
@@ -0,0 +1,7 @@
+package com.advertis.service;
+
+/**
+ * Created by Administrator on 2020/1/28 0028.
+ */
+public interface JedisClient {
+}
---
+++ pharmacy-advertising/src/main/java/com/advertis/service/impl/JedisClientCluster.java
@@ -0,0 +1,9 @@
+package com.advertis.service.impl;
+
+import com.advertis.service.JedisClient;
+
+/**
+ * Created by Administrator on 2020/1/28 0028.
+ */
+public class JedisClientCluster implements JedisClient {
+}
