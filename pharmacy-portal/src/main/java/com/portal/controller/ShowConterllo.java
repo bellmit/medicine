27fe0a49573b90581fbfcd2f2cf4de16daaca678
freedom-g <15502283132@163.com>
2020-02-03 14:28:43 +0800

    修改

--- pharmacy-portal/src/main/java/com/portal/controller/ShowConterllo.java
+++ pharmacy-portal/src/main/java/com/portal/controller/ShowConterllo.java
@@ -1,6 +1,9 @@
 package com.portal.controller;
 
+import com.portal.service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -8,8 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class ShowConterllo {
+
+    @Autowired
+    ContentService contentService;
+
     @RequestMapping("/")
-    public String show(){
+    public String show(Model model){
+        String adJson = contentService.getContentList();
+        model.addAttribute("ad1", adJson);
         return "index";
     }
 }
