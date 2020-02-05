1292ff744287bcc3edcb3bf21d8b81cdba592f41
freedom-g <15502283132@163.com>
2020-02-05 13:39:18 +0800

    修改

--- pharmacy-portal/src/main/java/com/portal/service/impl/ContentServiceImpl.java
+++ pharmacy-portal/src/main/java/com/portal/service/impl/ContentServiceImpl.java
@@ -1,9 +1,15 @@
 package com.portal.service.impl;
 
+import com.advertis.pojo.Contents;
 import com.portal.service.ContentService;
 import httpClient.HttpClientUtil;
 import json.JsonUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2020/2/1 0001.
@@ -12,7 +18,26 @@ import org.springframework.stereotype.Service;
 public class ContentServiceImpl implements ContentService {
     public String getContentList(){
 
-        return HttpClientUtil.doGet("http://localhost:8083");
-
+        String result = HttpClientUtil.doGet("http://localhost:8083/content");
+        try {
+        List<Contents> contentses = JsonUtils.jsonToList(result, Contents.class);
+        List<Map> resultList = new ArrayList<>();
+        //创建一个jsp页码要求的pojo列表
+        for (Contents tbContent : contentses) {
+            Map map = new HashMap<>();
+            map.put("src", tbContent.getPic());
+            map.put("height", 240);
+            map.put("width", 670);
+            map.put("widthB", 550);
+            map.put("heightB", 240);
+            map.put("href", tbContent.getUrl());
+            map.put("alt", tbContent.getTitle());
+            resultList.add(map);
+        }
+        return JsonUtils.objectToJson(resultList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
