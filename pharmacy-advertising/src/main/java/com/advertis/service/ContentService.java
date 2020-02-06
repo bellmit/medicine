fc2b4dbca7577c646653774ae71a2dfa51d3c770
freedom-g <15502283132@163.com>
2020-02-06 15:21:51 +0800

    修改

--- pharmacy-advertising/src/main/java/com/advertis/service/ContentService.java
+++ pharmacy-advertising/src/main/java/com/advertis/service/ContentService.java
@@ -1,7 +1,7 @@
 package com.advertis.service;
 
-import com.advertis.pojo.Contents;
-import com.advertis.pojo.Medicine;
+import com.advertis.pojo.Content;
+import com.advertis.pojo.MedicineMessage;
 
 import java.util.List;
 
@@ -11,8 +11,8 @@ import java.util.List;
 public interface ContentService {
 
     //查询content内容列表所有
-    public List<Contents> getContentList();
+    public List<Content> getContentList();
 
     //查询促销商品信息
-    public List<Medicine> getMedicineStatus();
+    public List<MedicineMessage> getMedicineStatus();
 }
