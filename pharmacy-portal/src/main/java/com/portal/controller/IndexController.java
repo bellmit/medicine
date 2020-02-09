55af347eb4a8f284f87cb7d7e4322468b4648970
freedom-g <15502283132@163.com>
2020-02-09 20:51:11 +0800

    indexController修改

--- pharmacy-portal/src/main/java/com/portal/controller/IndexController.java
+++ pharmacy-portal/src/main/java/com/portal/controller/IndexController.java
@@ -12,13 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @Autowired
-    private ContentService contentService;
     @RequestMapping("/")
-    public String showIndex(Model model){
-        String adJson = contentService.getContentList();
-        model.addAttribute("ad1", adJson);
-
+    public String showIndex(){
         return "index";
     }
 }
