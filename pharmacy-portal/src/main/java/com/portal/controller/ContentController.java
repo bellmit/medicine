b4d5ddd405197f6341f22c546e76f1da583b7cf3
freedom-g <15502283132@163.com>
2020-02-11 17:24:52 +0800

    contentController修改

--- pharmacy-portal/src/main/java/com/portal/controller/ContentController.java
+++ pharmacy-portal/src/main/java/com/portal/controller/ContentController.java
@@ -1,10 +1,15 @@
 package com.portal.controller;
 
 import com.portal.service.ContentService;
+import com.taotao.pojo.Content;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * Created by Administrator on 2020/2/9 0009.
@@ -16,11 +21,11 @@ public class ContentController {
    private ContentService contentService;
 
     @RequestMapping("/imgContent")
-    @ResponseBody
-    public String getList(){
-        String contentList = contentService.getContentList();
+    public String getList(HttpServletRequest request){
+        List<Content> contentList = contentService.getContentList();
+       request.getSession().setAttribute("content",contentList);
         System.out.println("123:"+contentList);
-        return contentList;
+        return "index";
     }
 
 }
