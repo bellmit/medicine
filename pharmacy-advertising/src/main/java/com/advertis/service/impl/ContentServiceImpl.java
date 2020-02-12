4927833a8c69c1c8addaa944d6dccdaa76c67032
freedom-g <15502283132@163.com>
2020-02-12 16:20:23 +0800

    Service增加促销商品方法

--- pharmacy-advertising/src/main/java/com/advertis/service/impl/ContentServiceImpl.java
+++ pharmacy-advertising/src/main/java/com/advertis/service/impl/ContentServiceImpl.java
@@ -2,6 +2,7 @@ package com.advertis.service.impl;
 
 import com.advertis.service.ContentService;
 import com.medince.mapper.ContentMapper;
+import com.medince.mapper.MedicineMessageMapper;
 import com.taotao.pojo.Content;
 import com.taotao.pojo.MedicineMessage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,19 +19,21 @@ public class ContentServiceImpl implements ContentService {
     @Autowired
     private ContentMapper contentMapper;
 
+    @Autowired
+    private MedicineMessageMapper messageMapper;
+
     @Override
     public List<Content> getContentList() {
-
-        System.out.println(2);
         List<Content> contents = contentMapper.selectAll();
-        System.out.println(1);
         System.out.println(contents);
         return  contents;
     }
 
     @Override
     public List<MedicineMessage> getMedicineStatus() {
-        return null;
+        List<MedicineMessage> medicineMessages = messageMapper.selectStatus();
+        System.out.println(medicineMessages);
+        return medicineMessages;
     }
 
 }
