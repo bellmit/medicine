3dacb2bd639564757ec33ac681fb58ddf85eb732
freedom-g <15502283132@163.com>
2020-02-12 16:09:11 +0800

    添加查询促销商品方法

--- medince-mapper/src/main/java/com/medince/mapper/MedicineMessageMapper.java
+++ medince-mapper/src/main/java/com/medince/mapper/MedicineMessageMapper.java
@@ -4,6 +4,7 @@ import com.taotao.pojo.MedicineMessage;
 import com.taotao.pojo.MedicineMessageExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 public interface MedicineMessageMapper {
     int countByExample(MedicineMessageExample example);
@@ -15,6 +16,10 @@ public interface MedicineMessageMapper {
     int insert(MedicineMessage record);
 
     int insertSelective(MedicineMessage record);
+
+    //新增查询商品状态方法
+    @Select("select * from medicine_message where status=1 limit 0,2")
+    List<MedicineMessage> selectStatus();
 
     List<MedicineMessage> selectByExample(MedicineMessageExample example);
 
